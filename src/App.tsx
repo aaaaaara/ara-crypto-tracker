19cdb0ea6e7e4cc463c0c889124e007f3e17ab9b
offdutyby <offdutybyblo@gmail.com>
2022-02-21 17:08:22 +0900

    feat: route setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
+import Home from "pages/Home/Home";
+import Login from "pages/Login/Login";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
 	return (
-		<div className="App">
-			<div>hello world</div>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/Login/*" element={<Login />} />
+				<Route path="/*" element={<div>잘못된 경로입니다.</div>} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
-export default App;
+export default Router;
