3a8dd275c12217603f2fcc2e1a9fbc9c92e85762
aaaaaara <araring.j@gmail.com>
2022-03-05 04:21:58 +0900

    wip

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -86,7 +86,7 @@ export default function Coin() {
   const loading = infoLoading || tickersLoading;
   
   const onClick = () => {
-    navigate(`${process.env.PUBLIC_URL}/`);
+    navigate(`${process.env.PUBLIC_URL}`);
   }
 
   const setDarkAtom = useSetRecoilState(isDarkState);
